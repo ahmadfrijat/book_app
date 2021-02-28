c880a12c9ee798af556880ae7f1be2f7328deaa1
ramayaser66 <ramayaser66@gmail.com>
2021-02-28 23:34:49 +0200

    lab-11-rama

--- server.js
+++ server.js
@@ -10,33 +10,59 @@ const PORT = process.env.PORT;
 app.use(cors());
 app.set('view engine', 'ejs')
 app.use(express.static('./public'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/',(req,res)=>{
     res.render('pages/index');
 });
+
+app.get('/error', (req,res)=>{
+    res.render('pages/error'); 
+}); 
+
 app.get('/searches/new',(req,res)=>{
     res.render('pages/searches/new');
 });
-app.get('/searches',handelSearches);
+app.post('/searches',handelSearches);
+
+app.get('*', (req,res)=>{
+    res.render('pages/error'); 
+
+});
+
+
 
 function handelSearches(req,res) {
+    
 
     const url =`https://www.googleapis.com/books/v1/volumes?q=${req.body.select}+${req.body.input}`
 // const url =`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor`
-superagent.get(url).then(data =>{
-console.log(data.body.items);
-let searchBook=data.body.items;
-let book=searchBook.map(data=>new Book(data));
-res.render('pages/searches/show')
 
-})    
+console.log(req.body);
+superagent.get(url).then(data =>{
+
+// console.log(data.body.items);
+let searchBook=data.body.items;
+let newBook=searchBook.map(data=>new Book(data));
+// console.log(newBook);
+res.render('pages/searches/show',{books:newBook})
+
+}).catch(error => {
+    res.render('/pages/error');
+
+});
 }
 function Book(data) {
-    this.title=data.body.items.volumeInfo.title;
-    this.authors=data.body.items.volumeInfo.authors;
-    this.image=data.body.items.volumeInfo.imageLinks;
-    this.decr=data.body.items.searchInfo.textSnippet;   
+    this.title=data.volumeInfo.title? data.volumeInfo.title: "No Title Available";
+    this.authors=data.volumeInfo.authors? data.volumeInfo.authors: "No Authors";;
+    this.image=data.volumeInfo.imageLinks.thumbnail? data.volumeInfo.imageLinks.thumbnail:"https://i.imgur.com/J5LVHEL.jpg";
+    this.decr=data.volumeInfo.description? data.volumeInfo.description:"No description available";   
 }
+
+let message = "SORRY YOU HAVE DO A MISTAKE"
+app.get('*', (req, res) => {
+    res.status(404).render('/pages/error', { 'message': message })
+});
 app.listen(PORT, ()=>{
     console.log('server is listinig to '+ PORT);
 });
